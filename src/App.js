80b689d48d3f4324f09520d3e856c101198822ed
kartik-tomar <kartik9896@gmail.com>
2020-06-17 20:07:35 +0530

    added language in localstorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { I18Provider, LOCALES } from "./i18n";
@@ -6,13 +6,26 @@ import { I18Provider, LOCALES } from "./i18n";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
-import { useState } from "react";
 
 function App() {
   const [locale, setLocale] = useState(LOCALES.ENGLISH);
+
+  useEffect(() => {
+    if (localStorage.getItem("language") !== null) {
+      setLocale(localStorage.getItem("language"));
+    } else {
+      localStorage.setItem("language", locale);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (localStorage.getItem("language") !== locale) {
+      localStorage.setItem("language", locale);
+    }
+  }, [locale]);
+
   return (
     <I18Provider locale={locale}>
-      {console.log("11")}
       <Router>
         <NavBar setLanguage={setLocale} language={locale} />
         <Route exact path="/" component={HomePage} />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,27 +4,24 @@ import translate from "../i18n/translate";
 
 import Poem from "../components/Poem";
 
-class HomePage extends Component {
-  render() {
-    return (
-      <div>
-        <div className="jumbotron jumbotron-fluid">
-          <div className="container">
-            <h1 className="display-1 text-center">
-              <strong>POETRY{console.log("111")}</strong>
-            </h1>
-            <p className="display-4 text-center">{translate("jumbotron2")}</p>
-          </div>
-        </div>
+const HomePage = () => {
+  return (
+    <div>
+      <div className="jumbotron jumbotron-fluid">
         <div className="container">
-          <h2 className="display-3 text-center m-2 mb-4">
-            {translate("TodayTop")}
-          </h2>
-          <Poem />
+          <h1 className="display-1 text-center">
+            <strong>POETRY</strong>
+          </h1>
+          <p className="display-4 text-center">{translate("jumbotron2")}</p>
         </div>
       </div>
-    );
-  }
-}
-
+      <div className="container">
+        <h2 className="display-3 text-center m-2 mb-4">
+          {translate("TodayTop")}
+        </h2>
+        <Poem />
+      </div>
+    </div>
+  );
+};
 export default HomePage;
